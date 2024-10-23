1c46eb1787ad034c6206333f6bed4ac473c3e892
taylor.wu <taylor.wu@appier.com>
2024-10-23 20:57:59 +0800

    initial commit

---
+++ src/components/Input/Input.types.ts
@@ -0,0 +1,12 @@
+import type { ChangeEventHandler } from "react";
+
+export interface InputProps {
+  id?: string;
+  label?: string;
+  error?: boolean;
+  message?: string;
+  success?: boolean;
+  disabled?: boolean;
+  placeholder?: string;
+  onChange?: ChangeEventHandler<HTMLInputElement>;
+}
